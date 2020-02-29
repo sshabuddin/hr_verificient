d47dc1425cbaea3d962f81fe91591d53e37032c1
sshabuddin <syedshabuddin@protonmail.com>
2020-02-29 23:40:25 +0530

    This is employe table -by aditya

---
+++ employe/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class employe(models.Model):
+   emp_id = models.IntegerField(primary_key=True)
+   emp_name = models.CharField(max_length = 50)
+   phonenumber = models.IntegerField()
+   joining_date = models.DateTimeField()
+   emp_email = models.CharField(max_length = 50)
+   reporting_manager = models.CharField(max_length = 50)
+   
+   class Meta:
+      db_table = "Employe"
